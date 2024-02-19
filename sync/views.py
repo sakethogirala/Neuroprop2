72e7127bbec2e92eb50011885f20f1e9287f47d1
Sam Suchin <samsuchin@gmail.com>
2024-02-19 15:31:28 -0500

    add body

--- sync/views.py
+++ sync/views.py
@@ -4,5 +4,6 @@ from django.views.decorators.csrf import csrf_exempt
 @csrf_exempt
 def email_webhook(request):
     if request.method == "POST":
-        print(request.POST)
+        print("post: ", request.POST)
+        print("body: ", request.body)
         return HttpResponse("Hello World")
