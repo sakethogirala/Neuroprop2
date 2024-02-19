fb7aa21f32b4c99ec7956e5474fa94ad18051da2
Sam Suchin <samsuchin@gmail.com>
2024-02-19 15:13:27 -0500

    csrf exempt

--- sync/views.py
+++ sync/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, HttpResponse
+from django.views.decorators.csrf import csrf_exempt
 
-
+@csrf_exempt
 def email_webhook(request):
     if request.method == "POST":
         print(request.POST)
